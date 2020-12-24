79d11dd67c355be3793add059227a6df093cf4a7
Cesar-Dev <cesarvargasescorcia@gmail.com>
2020-12-24 00:46:34 -0600

    EMP(DATA): preparando list-empleados.compponent traer los datos de firestore a la consola del navegador

--- src/app/components/list-empleados/list-empleados.component.ts
+++ src/app/components/list-empleados/list-empleados.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { EmpleadoService } from '../../service/empleado.service';
 
 @Component({
   selector: 'app-list-empleados',
@@ -8,12 +9,30 @@ import { Observable } from 'rxjs';
   styleUrls: ['./list-empleados.component.css']
 })
 export class ListEmpleadosComponent implements OnInit {
+  empleados: any[] =[];
 
 
-  constructor() { 
+  constructor(private _empleadoService: EmpleadoService) { 
   }
 
   ngOnInit(): void {
+    this.getEmpleados();
+  }
+
+  getEmpleados(){
+    this._empleadoService.getEmpleados().subscribe(data => {
+      //console.log(data);
+      this.empleados =[];
+      data.forEach((element:any) => {
+        //console.log(element.payload.doc.id);
+        //console.log(element.payload.doc.data());
+        this.empleados.push({
+          id: element.payload.doc.id,
+          ...element.payload.doc.data()
+        })
+      });
+      console.log(this.empleados);
+    })
   }
 
 }
--- src/app/service/empleado.service.ts
+++ src/app/service/empleado.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,8 @@ export class EmpleadoService {
 
    agregarEmpleado(empleado: any):Promise<any>{
      return this.firestore.collection('empleados').add(empleado);
+   }
+   getEmpleados(): Observable<any>{
+      return this.firestore.collection('empleados').snapshotChanges();
    }
 }
