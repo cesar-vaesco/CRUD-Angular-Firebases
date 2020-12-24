9c5d6b48a9391f6e20c34a66e7230cad89e4feee
Cesar-Dev <cesarvargasescorcia@gmail.com>
2020-12-23 18:17:27 -0600

    EMP(Routing): configurando rutas  - principio de SPA

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateEmpleadosComponent } from './components/create-empleados/create-empleados.component';
+import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /*Para inicializar nuestra vista directamente viendo la lista de usuarios*/
+  {path:'', redirectTo: 'lista-empleados', pathMatch: 'full'}, 
+  {path:'lista-empleados', component: ListEmpleadosComponent},
+  {path:'crear-empleado', component: CreateEmpleadosComponent},
+  /*En caso de no encontrar o ingresar una url erronea redirecciona a la vista lista-empleados*/ 
+  {path:'**', redirectTo: 'lista-empleados', pathMatch: 'full'}, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
