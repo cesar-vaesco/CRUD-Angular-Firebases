b6aa747dcc4e007732e5655c24b9483557c652ce
Cesar-Dev <cesarvargasescorcia@gmail.com>
2020-12-23 23:09:28 -0600

    EMP: Configurando el registro de llenado de formulario para persistir en FIRESTORE

--- src/app/components/create-empleados/create-empleados.component.ts
+++ src/app/components/create-empleados/create-empleados.component.ts
@@ -1,47 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmpleadoService } from '../../service/empleado.service';
 
 @Component({
   selector: 'app-create-empleados',
   templateUrl: './create-empleados.component.html',
-  styleUrls: ['./create-empleados.component.css']
+  styleUrls: ['./create-empleados.component.css'],
 })
 export class CreateEmpleadosComponent implements OnInit {
-/**
- * PErmite conectar con el formulario del html
- */
+  /**
+   * PErmite conectar con el formulario del html
+   */
   createEmpleado: FormGroup;
 
   submitted = false;
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    /*Se importa el servicio */ 
+    private _empleadoService: EmpleadoService
+  ) {
     this.createEmpleado = this.fb.group({
-      nombre:['', Validators.required],
-      apellido:['', Validators.required],
-      documento:['', Validators.required],
-      salario:['', Validators.required]
-    })
-   }
-
-  ngOnInit(): void {
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      documento: ['', Validators.required],
+      salario: ['', Validators.required],
+    });
   }
 
-  agregarEmpleado(){
-    /*Se cambia el estado de submmited*/ 
+  ngOnInit(): void {}
+
+  agregarEmpleado() {
+    /*Se cambia el estado de submmited*/
     this.submitted = true;
-    /*En caso de que al llenar el formulario los datos vayan vacios, el formulario se invalida */ 
-    if(this.createEmpleado.invalid){
+    /*En caso de que al llenar el formulario los datos vayan vacios, el formulario se invalida */
+    if (this.createEmpleado.invalid) {
       return;
     }
 
-    const empleado: any ={
+    const empleado: any = {
       nombre: this.createEmpleado.value.nombre,
       apellido: this.createEmpleado.value.apellido,
       documento: this.createEmpleado.value.documento,
-      salario: this.createEmpleado.value.documento,
+      salario: this.createEmpleado.value.salario,
       fechaCreacion: new Date(),
-      fechaActualizacion: new Date()
-    }
-    console.log(empleado);
+      fechaActualizacion: new Date(),
+    };
+    this._empleadoService.agregarEmpleado(empleado).then( () => {
+      console.log("Empleado registrado con éxito!!!!");
+    }).catch(error => {
+      console.log(error);
+    });
   }
-
 }
